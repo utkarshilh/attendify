59644c9c678a01bb319b69e3bd3ae930c4437ffe
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-03-29 20:32:56 +0530

    hello

--- src/Components/Attendence/FinalList.js
+++ src/Components/Attendence/FinalList.js
@@ -4,7 +4,35 @@ export default function FinalList({ attendanceRecords }) {
   console.log(JSON.stringify(attendanceRecords));
 
   return (
-    <div>FinalList</div>
+    <>
+      <h1>Final List</h1>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Roll No</th>
+            <th scope="col" colSpan={2}>First</th>
+            {/* <th scope="col">Last</th> */}
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+
+
+        <tbody>
+
+
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+
+
+        </tbody>
+      </table>
+    </>
+
+
+
 
 
   )
