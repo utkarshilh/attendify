27068d41d66e432d0abf6acac01bd805e6dcb2cd
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-03-31 13:26:25 +0530

    calling the component from the main component just to check

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Main from './Components/Attendence/Main';
+import SelectClass from './Components/Attendence/SelectClass';
 
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
+      <SelectClass />
     </div>
   );
 }
