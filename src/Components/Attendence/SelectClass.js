df2870f91f612a59f669d185277202b947643ecb
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-03-31 12:59:57 +0530

    new component is created to create the page where user is allowed to select the class and section

---
+++ src/Components/Attendence/SelectClass.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SelectClass() {
+    return (
+        <div>
+            <h1>This component is for selecting the class and section to take the attendance</h1>
+        </div>
+    )
+}
