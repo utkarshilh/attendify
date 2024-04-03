7863b7ef1f146e58adda7273d885a752b2a8dadc
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-04-03 18:46:01 +0530

    Created the conditional componet where it should direct the attendance main page after selectioon of the class and section even however the backend is still not working

--- src/Components/Conditional/IsClassSelected.js
+++ src/Components/Conditional/IsClassSelected.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
+import A from '../../A';
+import B from '../../B';
+import Main from '../Attendence/Main';
+import SelectClass from '../Attendence/SelectClass'
 
 export default function IsClassSelected() {
+    const [isClassSelected, setIsClassSelected] = useState(false);
+
+    const changeStatus = (currStatus) => {
+
+        setIsClassSelected(currStatus)
+    }
+
     return (
         <div>
-            <h1>Hello this is is class selected</h1>
+            {(isClassSelected === true) ? <Main /> : <SelectClass changeStatus={changeStatus} isClassSelected={isClassSelected} />}
+
         </div>
     )
 }
