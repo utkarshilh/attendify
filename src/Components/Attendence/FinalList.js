36d6bb99b25372103f41d5bd0905cfcbfbfcb32e
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-04-16 18:19:12 +0530

    dfsa

--- src/Components/Attendence/FinalList.js
+++ src/Components/Attendence/FinalList.js
@@ -1,9 +1,35 @@
+import axios from 'axios';
 import React from 'react';
+
 import { useState } from 'react';
 
-export default function FinalList({ attendanceRecords }) {
-
+export default function FinalList({ attendanceRecords, classs, section, subject }) {
   const [records, setRecords] = useState(attendanceRecords);
+
+  async function handleSubmit() {
+
+
+    if (records.length === 0) {
+      alert("No record to submit");
+      return;
+
+
+    }
+
+    try {
+      const response = await axios.post('http://localhost:5001/submitAttendance', { records, classs, subject, section }, { timeout: 5000 })
+
+      console.log(response)
+    } catch (error) {
+      console.log("this is not working ")
+
+    }
+    finally {
+      console.log("this is finally")
+      // this code will redirect to the previous
+    }
+
+  }
   const handleStatusButtonClick = (index) => {
     const newRecord = [...records];
 
@@ -12,6 +38,9 @@ export default function FinalList({ attendanceRecords }) {
     { (newRecord[index].status === 'present') ? newRecord[index].status = 'absent' : newRecord[index].status = 'present' }
 
     setRecords(newRecord);
+
+    console.log(records);
+
   }
   return (
     <>
@@ -35,7 +64,7 @@ export default function FinalList({ attendanceRecords }) {
           ))}
         </tbody>
       </table>
-      <button className='btn btn-primary btn-lg'>Submit</button>
+      <button className='btn btn-primary btn-lg' onClick={handleSubmit}>Submit</button>
     </>
   );
 }
