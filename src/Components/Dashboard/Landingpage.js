7bbad3e80c6a6aee0b6403fd52d1647d56d05ffa
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-04-26 23:23:45 +0530

    now added the take attendance to the vartical navbar function and it is working fine

--- src/Components/Dashboard/Landingpage.js
+++ src/Components/Dashboard/Landingpage.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './Dashboard'
 import Takeattendance from './Teacher/Takeattendance'
 import Viewattendance from './Teacher/Viewattendance'
+import IsClassSelected from '../Conditional/IsClassSelected';
 
 
 
@@ -21,7 +22,7 @@ export default function Landingpage() {
                 <div className="content">
                     <Routes>
                         <Route path="/" element={<Dashboard />} />
-                        <Route path="/take-attendance" element={<Takeattendance />} />
+                        <Route path="/take-attendance" element={<IsClassSelected />} />
                         <Route path="/view-attendance" element={<Viewattendance />} />
                     </Routes>
                 </div>
