b2094f0b6baea82f0a0d344b9b077365e7e8989d
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-04 22:12:47 +0530

    now working fine

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import IsClassSelected from './Components/Conditional/IsClassSelected';
 import Landingpage from './Components/Dashboard/Landingpage';
 import Login from './Components/LoginPage/Login';
 
 
-// import Main from './Components/Attendence/Main';
+
+
+
+import Main from './Components/Attendence/Main';
+
+import Navbar from './Components/Dashboard/Navbar';
 // import SelectClass from './Components/Attendence/SelectClass';
 
 
+
+
 function App() {
+  const [currUser, setCurrUser] = useState('');
+  const [userType, setUserType] = useState('');
+
+  const changeUser = ({ user, type }) => {
+    setCurrUser(user);
+    setUserType(type);
+  }
+
+
+
   return (
     <div className="App">
       {/* <IsClassSelected /> */}
       {/* <Landingpage /> */}
 
-      <Login />
+      {(currUser === '') ? <Login changeUser={changeUser} /> : <Landingpage />}
 
     </div>
   );
