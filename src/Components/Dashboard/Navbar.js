6aaa1c6fa1abf5406d780bc9e577834e875e3f1b
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2024-05-09 23:46:32 +0530

    reduced the width of sidebar

--- src/Components/Dashboard/Navbar.js
+++ src/Components/Dashboard/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
                 </svg>
             </button>
 
-            <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+            <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-30 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                 <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
                     <ul class="space-y-2 font-medium">
                         <li>
